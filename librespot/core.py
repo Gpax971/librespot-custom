55ac89b04b8b7696062572cfabc418504c139bf9
Gpax971 <gpaxalt@gmail.com>
2025-11-14 00:47:05 +1100

    Added print back

--- librespot/core.py
+++ librespot/core.py
@@ -164,6 +164,7 @@ class ApiClient(Closeable):
         mdb = self.get_ext_metadata(ExtensionKind.TRACK_V4, track.to_spotify_uri())
         md = Metadata.Track()
         md.ParseFromString(mdb)
+        print(md)
         return md
 
     def get_metadata_4_episode(self, episode: EpisodeId) -> Metadata.Episode:
